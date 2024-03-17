b9eed65630f3b0c79baea8c96b26cbd8c8f738ec
hdezerto <hugo.afonso.dezerto@gmail.com>
2024-03-17 17:13:25 +0000

    BLE (P2P)
    Bugs corrigidos.

--- Network/BLE (P2P)/auxiliary_node_1.ino
+++ Network/BLE (P2P)/auxiliary_node_1.ino
@@ -1,9 +1,15 @@
 
 
+// ESP32 Node (sensor node)
+// Code for BLE server
+
+
+#include <WiFi.h> // Include WiFi library (to turn it off)
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
@@ -11,12 +17,14 @@ BLECharacteristic* pAckCharacteristic = NULL; // New characteristic for the ACK
 bool deviceConnected = false;
 bool messageReceived = true;
 int messageNumber = 0;
+int maxMessages = 4;
 int retries = 0;
 int maxRetries = 3; // Define maxRetries
 unsigned long messageTimestamp = 0; // Timestamp when the message is sent
 unsigned long timeout = 10000; // Increased timeout to 10 seconds
 
-#define SERVICE_UUID           "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define NODE_NAME "ESP32_NODE_1"
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID_RX "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define CHARACTERISTIC_UUID_ACK "beb5483e-36e1-4688-b7f5-ea07361b26a9" // New UUID for the ACK characteristic
 
@@ -25,10 +33,12 @@ unsigned long timeout = 10000; // Increased timeout to 10 seconds
 class MyServerCallbacks: public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
       deviceConnected = true;
+      Serial.println("Device connected!");
     };
 
     void onDisconnect(BLEServer* pServer) {
       deviceConnected = false;
+      Serial.println("Device disconnected!");
     }
 };
 
@@ -47,7 +57,9 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 void setup() {
   Serial.begin(115200);
 
-  BLEDevice::init("ESP32_Node_1");
+  WiFi.mode(WIFI_OFF); // Turn off WiFi to save power
+
+  BLEDevice::init(NODE_NAME);
 
   pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
@@ -72,37 +84,45 @@ void setup() {
 
   pService->start();
 
-  pServer->getAdvertising()->start();
 }
 
 
+
 void loop() {
-  if (deviceConnected) {
-    Serial.println("Device connected!");
-    if (messageReceived) {
-      messageNumber++;
-      String message = "Message from Node 1: " + String(messageNumber);
-      Serial.println("Message " + String(messageNumber) + " sent, waiting acknowledgment");
-      pCharacteristic->setValue(message.c_str());
-      pCharacteristic->notify();
-      messageReceived = false;
-      retries = 0;  // Reset retries on successful message sent
-      messageTimestamp = millis(); // Save the timestamp when the message is sent
-    } else if (millis() - messageTimestamp > timeout) { // Check for timeout
-      if (retries < maxRetries) {
-        Serial.println("Message not acknowledged, resending...");
+  if (messageNumber < maxMessages) { // Check if there are messages to send
+    pServer->startAdvertising(); // Start advertising
+    if (deviceConnected) {
+      if (messageReceived) { // Send new message
+        messageNumber++;
+        String message = String(NODE_NAME) + ": Message " + String(messageNumber);
+        Serial.println("Message " + String(messageNumber) + " sent, waiting ACK");
         pCharacteristic->setValue(message.c_str());
         pCharacteristic->notify();
-        messageTimestamp = millis(); // Update the timestamp
-        retries++;
+        messageReceived = false;
+        retries = 0;  // Reset retries on successful message sent
+        messageTimestamp = millis(); // Save the timestamp when the message is sent
+      } else if (millis() - messageTimestamp > timeout) { // Check for timeout
+        if (retries < maxRetries) { // Resend message
+          Serial.println("Message not acknowledged, resending...");
+          String message = String(NODE_NAME) + ": Message " + String(messageNumber);
+          pCharacteristic->setValue(message.c_str());
+          pCharacteristic->notify();
+          messageTimestamp = millis(); // Update the timestamp
+          retries++;
+        } else {
+          Serial.println("Maximum retries reached, giving up on message");
+          // Handle the case where retries are exhausted (optional)
+          }
       } else {
-        Serial.println("Maximum retries reached, giving up on message");
-        // Handle the case where retries are exhausted (optional)
-        }
+        Serial.println("Waiting ACK");
+      }
+    } else {
+      Serial.println("No device connected to send message");
     }
   } else {
-    Serial.println("No device connected!");
+    Serial.println("No more messages to send");
   }
+    
   
-  delay(2000); // Reduced delay for faster reconnection attempts
+  delay(2000); // Delay for processing time at central node
 }
--- Network/BLE (P2P)/central_node.py
+++ Network/BLE (P2P)/central_node.py
@@ -1,57 +1,66 @@
 
-# Code for windows machine
+
+# Central node (Linux or Windows machine)
+# Code for BLE client
+
 
 import asyncio
-from bleak import BleakScanner, BleakClient
+from bleak import BleakScanner, BleakClient, BleakError
 
 service_uuid = "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 characteristic_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 ack_characteristic_uuid = "beb5483e-36e1-4688-b7f5-ea07361b26a9"
 
 
+async def connect_to_device(dev):
+    try:
+        async with BleakClient(dev.address) as client:
+            svc = client.services.get_service(service_uuid)
+            ch = svc.get_characteristic(characteristic_uuid)
+            ack_ch = svc.get_characteristic(ack_characteristic_uuid)
+
+            try:
+                data = await client.read_gatt_char(ch)
+                data_string = data.decode('utf-8')
+                print(data_string)
+            except Exception as e:
+                print(f"Error reading data: {e}")
+
+            try:
+                ack_written = await client.write_gatt_char(ack_ch, bytearray("ACK", "utf-8"))
+                print("ACK sent successfully")
+            except Exception as e:
+                print(f"Error writing ACK: {e}")
+
+    except BleakError as e:
+        print(f"Failed to connect to device {dev.address}: {e}")
+
+
+
+
 async def run():
     devices = await BleakScanner.discover()
 
-    if not devices:  # Check if any devices were found
+    if not devices:
         print("No Bluetooth devices found.")
         return
 
     for dev in devices:
         device_name = dev.name
-        if device_name is not None and device_name.startswith("ESP32_Node"):
+        if device_name is not None and device_name.startswith("ESP32_NODE"):
             print(f"Device {dev.address} ({dev.name}), RSSI={dev.rssi} dB")
+            try:
+                await asyncio.wait_for(connect_to_device(dev), timeout=10.0) # The connection has a timeout
+            except asyncio.TimeoutError:
+                print(f"Timeout while trying to connect to {dev.address}. Moving on to next device.")
+                continue
 
-            async with BleakClient(dev.address) as client:
-                try:
-                    svc = client.services.get_service(service_uuid)
-                    ch = svc.get_characteristic(characteristic_uuid)
-                    ack_ch = svc.get_characteristic(ack_characteristic_uuid)  # Get the ACK characteristic
-
-                    try:  # Handle potential error during data reading
-                        data = await client.read_gatt_char(ch)
-                        data_string = data.decode('utf-8')
-                        print("Sensor data:", data_string)
-                    except Exception as e:
-                        print(f"Error reading data: {e}")
-
-                    try:
-                        ack_written = await client.write_gatt_char(ack_ch, bytearray("ACK", "utf-8"))  # Write to the ACK characteristic
-                        if ack_written:
-                            print("Acknowledgement sent successfully")
-                        else:
-                            print("Failed to send acknowledgement!")
-                            # Handle the failure, e.g., log the error or retry with a delay
-                    except Exception as e:
-                        print(f"Error writing ACK: {e}")
-                        # Handle the exception, e.g., log the error or attempt to reconnect
-
-                except Exception as e:  # Catch potential connection errors
-                    print(f"Connection error: {e}")
-
-    print("No more devices found. Reconnecting...")
-    await asyncio.sleep(2)  # Wait 2 seconds before retrying
+    print("Scanning for devices...")
+    #await asyncio.sleep(2)
 
 
+
+# Run forever
 while True:
     loop = asyncio.get_event_loop()
     loop.run_until_complete(run())
