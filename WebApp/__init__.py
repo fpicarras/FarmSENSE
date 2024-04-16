830d1331c97680756f4bd8a44062d215c395d399
Matilde Sardinha <160400054+matildesardinha@users.noreply.github.com>
2024-04-16 23:26:05 +0100

    Add files via upload

---
+++ WebApp/__init__.py
@@ -0,0 +1,72 @@
+import io, base64
+import matplotlib.pyplot as plt
+from flask_bootstrap import Bootstrap4
+from flask import Flask, render_template,redirect, url_for, request, session
+
+from datetime import timedelta
+import client
+
+app = Flask(__name__)
+bootstrap = Bootstrap4(app)
+app.secret_key = "hello"
+
+app.permanent_session_lifetime = timedelta(minutes=5)
+
+
+#@app.route('/')
+#def home():
+#    return render_template('index.html')
+
+@app.route("/login", methods=["POST", "GET"])
+def login_user():
+    if request.method == "POST":
+        session.permanent = True
+        user = request.form["username"]
+        password = request.form["password"]
+
+        user_id= client.login(user, password)
+        if user_id:
+            session["user"]=user_id
+            return redirect(url_for("user"))
+        else:
+            return redirect(url_for("login"))
+
+    else:
+        if "user" in session:
+            return redirect(url_for("user"))
+
+        return render_template("login.html")
+
+@app.route("/user", methods=['GET', 'POST'])
+def user():
+    if "user" in session:
+        if request.method == "POST":
+            node = request.form["sub_button"]   
+
+            vbat=client.plot_sensor_data(client.get_sensor_data(session["user"], node, 7))
+
+            buffer = io.BytesIO()
+            plt.savefig(buffer, format='png')
+            buffer.seek(0)
+            image_png = buffer.getvalue()
+            buffer.close()
+
+            plot_encoded = base64.b64encode(image_png).decode('utf-8')
+            return render_template("graphics.html", plot_encoded=plot_encoded, value=vbat)
+        else: 
+            return render_template("nodes.html")
+    else:
+        return redirect(url_for("login_user"))
+
+@app.route("/logout")
+def logout():
+	session.pop("user", None)
+	return redirect(url_for("login"))
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
+
+
