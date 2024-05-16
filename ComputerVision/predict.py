60ac30dcb1d0b6e811cbf630913f72cab502a1e0
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-05-16 12:17:59 +0100

    is_tomato recebe uma imagem e a localização do modelo. Retoma a imagem anotada e um dicionário com o número de tomátes de cada tipo encontrados

--- ComputerVision/predict.py
+++ ComputerVision/predict.py
@@ -8,10 +8,16 @@ def is_tomato(in_image, model_path):
     model = YOLO(model_path)
 
     results = model(in_image)
+    detect = {}
     for result in results:
-        im = result.plot()
+        im = result.plot(show = True)
+        print(result.names)
+        for j in result.names:
+            detect[result.names[j]] = 0
 
-    return im
+        for i in result.boxes.cls:
+            detect[result.names[int(i.item())]] +=1
+    return im, detect
 
 if __name__ == "__main__" :
     model_path = './tomato.pt'
@@ -20,7 +26,8 @@ if __name__ == "__main__" :
     if in_image is None:
         print('Could not open image')
         exit
-    image = is_tomato(in_image, model_path)
+    image, objetos = is_tomato(in_image, model_path)
+    print(objetos)
     cv2.imshow('Output', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
