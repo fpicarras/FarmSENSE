b07dd94040770597cdc9020ef88e626581c0aea3
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-05-16 11:41:12 +0100

    Função is_tomato devolve a imagem devidamente anotada

--- ComputerVision/predict.py
+++ ComputerVision/predict.py
@@ -3,11 +3,11 @@ from ultralytics import YOLO
 import cv2
 import sys
 
-def is_tomato(image_path, model_path):
+def is_tomato(in_image, model_path):
 
     model = YOLO(model_path)
 
-    results = model(image_path)
+    results = model(in_image)
     for result in results:
         im = result.plot()
 
@@ -16,7 +16,11 @@ def is_tomato(image_path, model_path):
 if __name__ == "__main__" :
     model_path = './tomato.pt'
     image_path = sys.argv[1]
-    image = is_tomato(image_path, model_path)
+    in_image = cv2.imread(image_path)
+    if in_image is None:
+        print('Could not open image')
+        exit
+    image = is_tomato(in_image, model_path)
     cv2.imshow('Output', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
