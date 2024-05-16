022c7245b55bc64761405e2b358ccd6162f65255
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-05-16 11:24:26 +0100

    predict.py works?' '

--- ComputerVision/predict.py
+++ ComputerVision/predict.py
@@ -1,6 +1,5 @@
 from ultralytics import YOLO
 
-from PIL import Image
 import cv2
 import sys
 
@@ -9,24 +8,16 @@ def is_tomato(image_path, model_path):
     model = YOLO(model_path)
 
     results = model(image_path)
-    # Read the input image
-    image = cv2.imread(image_path)
-    # Annotate the detected objects on the image
     for result in results:
-        print(result)
-#        print(result.boxes)
-#        print(result.masks)
-        im
-#        print(im)
-        cv2.imshow('YOLO', im)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+        im = result.plot()
 
-    return image
+    return im
 
 if __name__ == "__main__" :
     model_path = './tomato.pt'
     image_path = sys.argv[1]
-    is_tomato(image_path, model_path)
-
+    image = is_tomato(image_path, model_path)
+    cv2.imshow('Output', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
