9d44a837bcf54adbc89e726e4fdf60badd072e3c
WoneBone <nuno.g.tribolet.de.abreu@tecnico.ulisboa.com>
2024-06-25 16:43:40 +0100

    Modelo em .json e comentários em predict

--- ComputerVision/predict.py
+++ ComputerVision/predict.py
@@ -6,6 +6,9 @@ import threading
 from datetime import datetime 
 import os
 import json
+
+#General detection function
+#Returns an anotated image and a dictionary with the objects detected
 def is_tomato(in_image, model_path):
 
     model = YOLO(model_path)
@@ -22,16 +25,23 @@ def is_tomato(in_image, model_path):
             detect[result.names[int(i.item())]] +=1
     return im, detect
 
+#Thread function
+#Runs the detection and saves the image and a .json file with the details
 def tomato_thread(in_image, UID, model_path):
     out_image, detect = is_tomato(in_image, model_path)
     print(detect)
     date = datetime.now()
-    name = date.strftime("%d-%m-%Y_%H-%M-%S")
+    name = date.strftime("%d-%m-%Y_%H-%M-%S")   #File name is the time
+
+    #Creates user id directory
     if not os.path.exists(UID):
         os.mkdir(UID)
-    cv2.imwrite(UID + '/' + name + ".png", out_image)
+    cv2.imwrite(UID + '/' + name + ".png", out_image)   #Write image file
+
     with open(UID + '/' + name + '.json', "w") as outfile: 
-        json.dump(detect, outfile)
+        json.dump({"model":  model_path, "detections" : detect}, outfile)   #Write .json file
+
+#Runs the detection on an image in a thread
 def detect(in_image, UID, model_path):
     t = threading.Thread(target = tomato_thread, args = (in_image, UID, model_path))
 
